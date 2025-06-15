db7bf28616bbf5d1ba62987f0079f4dad73a4ece
Neo <54811660+neooriginal@users.noreply.github.com>
2025-06-15 13:31:20 +1000

    security improvements

--- Friend/public/js/main.js
+++ Friend/public/js/main.js
@@ -48,7 +48,7 @@ const navigate = (page) => {
 const setCookie = (name, value, days = 7) => {
     const date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
+    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/;SameSite=Strict;Secure=${location.protocol === 'https:'}`;
 };
 
 const getCookie = (name) => {
